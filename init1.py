b5c369b89287dba41c70acf5b69644e4dc641fcf
Holly1212 <zh2667@nyu.edu>
2022-11-30 17:07:46 -0500

    change user and password

--- init1.py
+++ init1.py
@@ -18,8 +18,8 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 # Configure MySQL
 conn = pymysql.connect(host='localhost',
                        port=3306,
-                       user='holly',
-                       password='hzs1212',
+                       user='root',
+                       password='root',
                        db='Cookzilla',
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor)
