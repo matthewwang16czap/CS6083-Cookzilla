b65e8be0cfeecf529bf1983d5a6144af042099aa
zcw8585 <113261031+zcw8585@users.noreply.github.com>
2022-12-15 16:35:49 -0500

    Delete sql_statements directory

--- sql_statements/avgstars_view.sql
+++
@@ -1,8 +0,0 @@
-create view recipe_avgstars as
-select r.recipeID, r.title, r.numServings, r.postedBy, IFNULL(a.avgstars, 0) as avgstars
-from recipe r left join (
-	select recipeID, avg(stars) as avgstars
-    from review
-	group by recipeID
-) a on r.recipeID = a.recipeID
-;
