c7b8bad1513d0a0243b4db14c23afcd060877d9e
zcw8585 <113261031+zcw8585@users.noreply.github.com>
2022-12-16 17:49:15 -0500

    Update unitconversion.sql

--- sql_statements/unitconversion.sql
+++ sql_statements/unitconversion.sql
@@ -43,7 +43,30 @@ values ('fl oz', 'pt',0.0520421);
 insert into unitconversion (sourceUnit, destinationUnit, ratio)
 values ('pt', 'fl oz',19.2152);
 
-g, mg, kg, oz, lb
+insert into unitconversion (sourceUnit, destinationUnit, ratio)
+values ('l', 'ml',1000);
+insert into unitconversion (sourceUnit, destinationUnit, ratio)
+values ('ml', 'l',0.001);
+insert into unitconversion (sourceUnit, destinationUnit, ratio)
+values ('l', 'fl oz',33.814);
+insert into unitconversion (sourceUnit, destinationUnit, ratio)
+values ('fl oz', 'l',0.0295735);
+insert into unitconversion (sourceUnit, destinationUnit, ratio)
+values ('l', 'pt',1.75975);
+insert into unitconversion (sourceUnit, destinationUnit, ratio)
+values ('pt', 'l',0.568261);
+insert into unitconversion (sourceUnit, destinationUnit, ratio)
+values ('ml', 'fl oz',0.033814);
+insert into unitconversion (sourceUnit, destinationUnit, ratio)
+values ('fl oz', 'ml',29.5735);
+insert into unitconversion (sourceUnit, destinationUnit, ratio)
+values ('ml', 'pt',0.00175975);
+insert into unitconversion (sourceUnit, destinationUnit, ratio)
+values ('pt', 'ml',568.261);
+insert into unitconversion (sourceUnit, destinationUnit, ratio)
+values ('fl oz', 'pt',0.0520421);
+insert into unitconversion (sourceUnit, destinationUnit, ratio)
+values ('pt', 'fl oz',19.2152);
 
 insert into unitconversion (sourceUnit, destinationUnit, ratio)
 values ('g', 'mg',1000);
