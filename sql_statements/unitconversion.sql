4ae4093a49a227a0459b4dd49ae2ca6cf90cb422
SiHanasaku <91296664+matthewwang16czap@users.noreply.github.com>
2022-12-15 20:44:00 -0500

    please someone fill the permutation

--- sql_statements/unitconversion.sql
+++ sql_statements/unitconversion.sql
@@ -21,7 +21,11 @@ values ('lb');
 insert into unitconversion (sourceUnit, destinationUnit, ratio)
 values ('l', 'ml',1000);
 insert into unitconversion (sourceUnit, destinationUnit, ratio)
+values ('l', 'pt',1.76);
+insert into unitconversion (sourceUnit, destinationUnit, ratio)
 values ('fl oz', 'ml',29.574);
+insert into unitconversion (sourceUnit, destinationUnit, ratio)
+values ('fl oz', 'l',29574);
 insert into unitconversion (sourceUnit, destinationUnit, ratio)
 values ('pt', 'fl oz',16);
 insert into unitconversion (sourceUnit, destinationUnit, ratio)
@@ -29,16 +33,12 @@ values ('g', 'mg',1000);
 insert into unitconversion (sourceUnit, destinationUnit, ratio)
 values ('kg', 'g',1000);
 insert into unitconversion (sourceUnit, destinationUnit, ratio)
-values ('oz', 'g',28.35);
-insert into unitconversion (sourceUnit, destinationUnit, ratio)
-values ('lb', 'oz',16);
-insert into unitconversion (sourceUnit, destinationUnit, ratio)
-values ('fl oz', 'l',29574);
-insert into unitconversion (sourceUnit, destinationUnit, ratio)
-values ('l', 'pt',1.76);
-insert into unitconversion (sourceUnit, destinationUnit, ratio)
 values ('kg', 'oz',35.274);
 insert into unitconversion (sourceUnit, destinationUnit, ratio)
 values ('kg', 'lb',2.2);
+insert into unitconversion (sourceUnit, destinationUnit, ratio)
+values ('oz', 'g',28.35);
+insert into unitconversion (sourceUnit, destinationUnit, ratio)
+values ('lb', 'oz',16);
 insert into unitconversion (sourceUnit, destinationUnit, ratio)
 values ('lb', 'g',453.592);
